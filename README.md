6f12f41601159485db5fe54382a1d5199f45867e
Ben Williams <benj@minwilliams.net>
2016-03-06 20:14:06 +1100

    added readme, added coments

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quick Palette
+
+A (very) simple piece of Javascript that outputs some boxes on a page. Useful for creating colour palettes.
+
+
+## To run
+
+Download `index.html` and `script.js` then open in browser.
+
--- script.js
+++ script.js
@@ -5,10 +5,20 @@ var quickPalette = (function(){
   var col3 = ["4C6172", "332C2C","1D2226", "58593B", "717252"];
   var col4 = ["FCDB8E", "A68155","644531", "3E3A31", "877E6F"];
 
+  /**
+   * Creates a div with background set to hex code supplied
+   * @param {string} hex  - Hex colour code excluding the #
+   * @returns {string}
+   */
   function addEl(hex){
     return "<div class='box' style='background-color:#" + hex + ";'></div>";
   }
 
+  /**
+   * Creates a row of palette colours and add an ending div at the end
+   * @param {Array} arr  of hex codes
+   * @param el DOM element to add new divs into
+   */
   function buildLine(arr, el){
     for(var i= 0; i < arr.length; i++ ){
       el.innerHTML += addEl(arr[i]);
@@ -16,6 +26,9 @@ var quickPalette = (function(){
     el.innerHTML += "<div class='end'></div>";
   }
 
+  /**
+   * Loads all palette colours by building a row of colours at a time.
+   */
   function init(){
     var el = document.getElementById('palette');
     buildLine(col1, el);
